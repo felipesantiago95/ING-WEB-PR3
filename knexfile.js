88f434eddd8fa7e10a00ace9a6cc0b46566d73fc
felipesantiago95 <felipe.palan@udla.edu.ec>
2024-07-18 00:24:40 -0500

    Initial commit

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL || {
-      host: 'dpg-cqc9pkuehbks738bkfs0-a.oregon-postgres.render.com',
+      host: 'dpg-cqc9pkuehbks738bkfs0-a',
       user: 'fpalan95',
       password: '9e0xEHZBzCMMsZRYkwlxN4XbDEvACp0z',
       database: 'ingweb'
